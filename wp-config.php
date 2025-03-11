1bca84916f1bf6769b1dedb29a00119b7ccbbe93
havaian <m.usman.work@gmail.com>
2025-03-11 07:33:26 +0100

    Wordpress is working

---
+++ wp-config.php
@@ -0,0 +1,100 @@
+<?php
+/**
+ * WordPress Configuration File
+ */
+
+// Hardcode the website URLs without port
+define('WP_HOME', 'http://students.uz');
+define('WP_SITEURL', 'http://students.uz');
+
+// Force WordPress to use these URLs for everything
+define('WP_CONTENT_URL', 'http://students.uz/wp-content');
+
+// This prevents WordPress from doing any redirects
+define('FORCE_SSL_ADMIN', false);
+
+// Prevent port detection/redirects
+if (strpos($_SERVER['HTTP_HOST'], ':') !== false) {
+    list($host) = explode(':', $_SERVER['HTTP_HOST']);
+    $_SERVER['HTTP_HOST'] = $host;
+}
+
+// Tell WordPress it's behind a proxy
+if (isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] === 'https') {
+    $_SERVER['HTTPS'] = 'on';
+}
+
+// ** Database settings - You can get this info from your web host ** //
+/** The name of the database for WordPress */
+define('DB_NAME', 'wordpress');
+
+/** Database username */
+define('DB_USER', 'wordpress');
+
+/** Database password */
+define('DB_PASSWORD', 'wordpress_password');
+
+/** Database hostname */
+define('DB_HOST', 'db');
+
+/** Database charset to use in creating database tables. */
+define('DB_CHARSET', 'utf8');
+
+/** The database collate type. Don't change this if in doubt. */
+define('DB_COLLATE', '');
+
+/**#@+
+ * Authentication unique keys and salts.
+ *
+ * Change these to different unique phrases! You can generate these using
+ * the {@link https://api.wordpress.org/secret-key/1.1/salt/ WordPress.org secret-key service}.
+ *
+ * You can change these at any point in time to invalidate all existing cookies.
+ * This will force all users to have to log in again.
+ *
+ * @since 2.6.0
+ */
+define('AUTH_KEY',         'put your unique phrase here');
+define('SECURE_AUTH_KEY',  'put your unique phrase here');
+define('LOGGED_IN_KEY',    'put your unique phrase here');
+define('NONCE_KEY',        'put your unique phrase here');
+define('AUTH_SALT',        'put your unique phrase here');
+define('SECURE_AUTH_SALT', 'put your unique phrase here');
+define('LOGGED_IN_SALT',   'put your unique phrase here');
+define('NONCE_SALT',       'put your unique phrase here');
+
+/**#@-*/
+
+/**
+ * WordPress database table prefix.
+ *
+ * You can have multiple installations in one database if you give each
+ * a unique prefix. Only numbers, letters, and underscores please!
+ */
+$table_prefix = 'wp_';
+
+/**
+ * For developers: WordPress debugging mode.
+ *
+ * Change this to true to enable the display of notices during development.
+ * It is strongly recommended that plugin and theme developers use WP_DEBUG
+ * in their development environments.
+ *
+ * For information on other constants that can be used for debugging,
+ * visit the documentation.
+ *
+ * @link https://wordpress.org/documentation/article/debugging-in-wordpress/
+ */
+define('WP_DEBUG', false);
+
+/* Add any custom values between this line and the "stop editing" line. */
+
+/* That's all, stop editing! Happy publishing. */
+
+/** Absolute path to the WordPress directory. */
+if (!defined('ABSPATH')) {
+    define('ABSPATH', __DIR__ . '/');
+}
+
+/** Sets up WordPress vars and included files. */
+require_once ABSPATH . 'wp-settings.php';
